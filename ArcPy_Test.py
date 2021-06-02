d43e19082db58d65d36cbaaa2c2ba0a8012ae5e3
stfeirer <stfeirer@ucanr.edu>
2021-06-02 11:28:22 -0700

    reorder repo

--- ArcPy_Test.py
+++
@@ -1,6 +0,0 @@
-import arcpy
-tableName = "D:/users/stfeirer/Documents/ArcGIS/Projects/CalAdaptPy_Demo/CalAdaptPy_Demo.gdb/GetDataAPI"
-if arcpy.Exists(tableName) == False:
-    print(False)
-else:
-    print(True)
